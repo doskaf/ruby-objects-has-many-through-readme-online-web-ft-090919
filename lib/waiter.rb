2952bf4d3235144c3f1ba3d47266ad1fa613c2f5
Dominique Skaf <doskaf@yahoo.com>
2019-09-24 16:44:51 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,5 +1,17 @@
 class Waiter
   
   attr_accessor :name, :experience
+  
+  @@all = []
+
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 
 end
