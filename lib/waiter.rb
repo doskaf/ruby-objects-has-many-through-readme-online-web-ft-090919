fe4f0468f66eeb45356ae72a617cb3935cbe04bb
Dominique Skaf <doskaf@yahoo.com>
2019-09-24 16:53:55 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -25,5 +25,12 @@ class Waiter
   def customers
     meals.map {|meal| meal.customer}
   end
+  
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+    best_tipped_meal.customer
+  end
 
 end
